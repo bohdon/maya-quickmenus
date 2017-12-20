9048cb62b47e44b3b5f0f824a5bb5cbb8a50fd8a
Bohdon Sayre <bohdon@gmail.com>
2017-12-20 00:54:26 -0600

    adds release command to setup script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
+PROJECT_NAME="maya-quickmenus"
 PACKAGE_NAME="quickmenus"
 
 
@@ -20,6 +21,15 @@ build() {
     mkdir -p build
     cp -R src/$PACKAGE_NAME build/
     cp -R src/$PACKAGE_NAME.mod build/
+}
+
+release() {
+    clean
+    build
+    VERSION=`git describe --tags --abbrev=0`
+    cd build
+    zip -rX ${PROJECT_NAME}_${VERSION}.zip *
+    echo ${PROJECT_NAME}_${VERSION}.zip
 }
 
 clean() {
